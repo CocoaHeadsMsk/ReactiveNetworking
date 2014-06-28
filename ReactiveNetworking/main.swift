6a2840687f5cb181b3fea0c880007eba9731da4b
Dmitry Obukhov <stel2k@gmail.com>
2014-06-28 15:07:02 +0400

    Initial project import

---
+++ ReactiveNetworking/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ReactiveNetworking
+//
+//  Created by Contento on 28/06/14.
+//  Copyright (c) 2014 CocoaHeadsMsk. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
